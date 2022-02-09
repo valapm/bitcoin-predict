ef6b770b8b18ce8f92261ff583065ff38d0ffe36
Merlin <zezbvk@frenmail.com>
2022-02-09 14:12:30 +0100

    Expose contracts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import * as merkleTree from "./merkleTree"
 import * as hex from "./hex"
 import * as oracle from "./oracle"
 import * as sha from "./sha"
+import * as contracts from "./contracts"
 import { bsv } from "scryptlib"
 import * as rabin from "rabinsig"
 
-export { bsv, rabin, transaction, pm, lmsr, merkleTree, hex, oracle, sha }
+export { bsv, rabin, transaction, pm, lmsr, merkleTree, hex, oracle, sha, contracts }
